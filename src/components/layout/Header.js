f28f83dceb41262399f70497ca3bb219b97450bb
Muhammad <dev.muhammad1994@gmail.com>
2023-01-04 23:23:18 +0300

    Install / Add fontawesome icons

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,23 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faCloudSun,
+    faLocationDot,
+    faClockFour,
+} from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
     return (
         <header className="header">
             <div className="header__logo">
-                {/* Weather icon */}
+                <FontAwesomeIcon icon={faCloudSun} size="3x" />
                 <span>weatherly.io</span>
             </div>
             <form className="header__search">
-                {/* Location icon */}
-                <input type="text" maxLength='15' placeholder='Enter zip code or city here...' />
+                <FontAwesomeIcon icon={faLocationDot} />
+                <input
+                    type="text"
+                    maxLength="15"
+                    placeholder="Enter zip code or city here..."
+                />
             </form>
             <div className="header__info">
-                {/* Clock icon */}
+                <FontAwesomeIcon icon={faClockFour} />
                 <span className="header__day">Sunday</span>
                 <span className="header__time">9:31 PM</span>
             </div>
         </header>
-    )
+    );
 }
 
 export default Header;
