4b809db79ba5ef400e04f502f14ddda3e60da613
Muhammad <dev.muhammad1994@gmail.com>
2023-01-06 22:26:39 +0300

    Add WeatherSummary cmp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Header from './components/layout/Header';
+import WeatherSummary from './components/layout/weatherSummary/WeatherSummary';
 
 function App() {
   return <>
     <Header />
+    <WeatherSummary />
   </>
 }
 
---
+++ src/components/layout/weatherSummary/WeatherSummary.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function WeatherSummary() {
+    return (
+        <div>
+            <section>
+                {/* Icon here */}
+                <h1>70 &deg;</h1>
+            </section>
+            <section>
+                <p>Clear with periodic clouds</p>
+                <h1>{/* Icon here */} Fort Lauderdale, Fl</h1>
+            </section>
+        </div>
+    )
+}
+
+export default WeatherSummary;
