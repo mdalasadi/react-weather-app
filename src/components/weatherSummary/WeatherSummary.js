a2faadcf3b96b4595e0dd1e55e4461b6d22afe81
Muhammad <dev.muhammad1994@gmail.com>
2023-01-08 22:55:10 +0300

    Add styling to the WeatherSummary cmp

--- src/components/weatherSummary/WeatherSummary.js
+++ src/components/weatherSummary/WeatherSummary.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLocationDot, faCloudSun } from '@fortawesome/free-solid-svg-icons';
+import styles from './WeatherSummary.module.css';
 
 function WeatherSummary() {
     return (
-        <div>
-            <section>
+        <div className={styles['summary']} >
+            <section className={styles['summary__degree']}>
                 <FontAwesomeIcon icon={faCloudSun} />
-                <h1>70 &deg;</h1>
+                <h1>70&deg;</h1>
             </section>
-            <section>
+            <section className={styles['summary__location']}>
                 <p>Clear with periodic clouds</p>
-                <h1><FontAwesomeIcon icon={faLocationDot} /> Fort Lauderdale, Fl</h1>
+                <h1><FontAwesomeIcon icon={faLocationDot} /> Fort Lauderdale,Fl</h1>
             </section>
         </div>
     )
