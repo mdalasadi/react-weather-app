7338c470f6423194913dfcfc380365b23f9d48a2
Muhammad <dev.muhammad1994@gmail.com>
2023-01-13 05:34:47 +0300

    Adjust TemperaturesChart cmp

--- src/components/temperaturesChart/TemperaturesChart.js
+++ src/components/temperaturesChart/TemperaturesChart.js
@@ -24,61 +24,60 @@ ChartJS.register(
     Legend
 );
 
-
-const options = {
-    responsive: true,
-    maintainAspectRatio: false,
-    plugins: {
-        legend: {
-            display: false,
-            position: 'top',
-        },
-        title: {
-            display: false,
-        },
-    },
-    scales: {
-        y: {
-            grid: {
+function TemperaturesChart() {
+    const options = {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+            legend: {
                 display: false,
+                position: 'top',
             },
-            border: {
+            title: {
                 display: false,
             },
         },
-        x: {
-            grid: {
-                display: false,
+        scales: {
+            y: {
+                grid: {
+                    display: false,
+                },
+                border: {
+                    display: false,
+                },
             },
-            border: {
-                display: false,
+            x: {
+                grid: {
+                    display: false,
+                },
+                border: {
+                    display: false,
+                },
+            }
+        },
+        elements: {
+            line: {
+                tension: 0.5,
+                borderColor: '#65389f',
+            },
+            point: {
+                backgroundColor: '#65389f',
+                borderWidth: 2.5,
+                borderColor: '#fff',
             },
         }
-    },
-    elements: {
-        line: {
-            tension: 0.5,
-            borderColor: '#65389f',
-        },
-        point: {
-            backgroundColor: '#65389f',
-            borderWidth: 2.5,
-            borderColor: '#fff',
-        },
-    }
-};
+    };
 
-const data = {
-    labels: ['10PM', '1AM', '4AM', '7AM', '10AM', '1PM', '4PM', '7PM'],
-    datasets: [
-        {
-            label: 'Temperature',
-            data: [70, 68, 72, 74, 74, 73, 76, 71],
-        },
-    ],
-};
+    const data = {
+        labels: ['10PM', '1AM', '4AM', '7AM', '10AM', '1PM', '4PM', '7PM'],
+        datasets: [
+            {
+                label: 'Temperature',
+                data: [70, 68, 72, 74, 74, 73, 76, 71],
+            },
+        ],
+    };
 
-function TemperaturesChart() {
     return <div className={styles['chart']}>
         <Line options={options} data={data} width={100} height={120} />
     </div>;
