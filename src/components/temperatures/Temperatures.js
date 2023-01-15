c6afe62310d81ddee3afc7f3cfe035dd350610c4
Muhammad <dev.muhammad1994@gmail.com>
2023-01-15 22:35:10 +0300

    Change icons for next five days forecast

--- src/components/temperatures/Temperatures.js
+++ src/components/temperatures/Temperatures.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCloudSun } from '@fortawesome/free-solid-svg-icons';
 import styles from './Temperatures.module.css';
 
 function Temperatures(props) {
     return <div className={`${styles["card"]} ${styles["card--dark"]} ${styles["days"]}`}>
         {props.temps.map(temp => <div key={temp.day}>
             <p className={styles["days__temperature"]}>{temp.temperature}</p>
-            <FontAwesomeIcon icon={faCloudSun} size="3x" />
+            <img src={`http://openweathermap.org/img/w/${temp.icon}.png`} alt="" />
             <p className={styles["days__day"]}>{temp.day}</p>
         </div>)}
     </div>
